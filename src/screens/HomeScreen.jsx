3ba8302635a7d9f0b50b8267765185be18818504
Prashant Mishra <prashantmishra111@outlook.com>
2024-03-07 01:05:09 +0530

    pkg added

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import {Text, View} from "react-native";
+
+const HomeScreen = () => {
+
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Home Screen !</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
+
+// Screens-
+// 1. Main Home screen (Workout).
+// 2. Activity Screen (Reports).
+// 3. Activity detail Page (Workout detail Page).
+// 4. Profile.
